2b6cf03bcb399beda79ce3ca803804a96a5e465e
Andjar <andjaradji.rooseno@gmail.com>
2020-02-08 17:19:46 +0700

    init_project : setup dependencies

---
+++ app/src/main/java/rds/app/android/com/mygithubapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package rds.app.android.com.mygithubapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
